dc647513e8178363707eb4f58845ccc57801a32e
Ken <saganawski@gmail.com>
2020-07-22 16:02:19 -0500

    display all trainings

--- src/main/resources/static/recruitment/training/js/training.js
+++ src/main/resources/static/recruitment/training/js/training.js
@@ -1,46 +1,63 @@
 $(document).ready(function(){
     console.log("hits");
-//    $('#trainer-table').DataTable({
-//            ajax:{
-//                "url": "/trainings",
-//                "dataSrc": ""
-//            },
-//            columns :[
-//                {"data": "id" },
-//                {"data" : function(data,type,row,meta){
-//                    let firstName = data.application.firstName;
-//                    let lastName = data.application.lastName;
-//                    let applicantName = lastName + " ," + firstName;
-//                    return applicantName;
-//                    },
-//                    "defaultContent": ""},
-//                {"data" : function(data,type,row,meta){
-//                    if(data.scheduledTime == null){
-//                        return " ";
-//                    }
-//                    return moment(data.scheduledTime).format('YYYY-MM-DD h:mm:ss a');
-//                    },
-//                    "defaultContent": ""},
-//                {"data" : function(data,type,row,meta){
-//                        if(data.interviewers == null){
-//                            return " ";
-//                        }
-//                        let interviewersLastNames = data.interviewers.map(interviewer => interviewer.lastName);
-//                        return interviewersLastNames;
-//                    },
-//                    "defaultContent":""},
-//                {"data" : function(data, type,row,meta){
-//                    return moment(data.createdDate).format('YYYY-MM-DD h:mm:ss a');
-//                    }
-//                },
-//                {   "targets": -1,
-//                    "data": function(data, type,row,meta){
-//                        return '<a class="btn btn-warning" href="/recruitment/interview/interview-details.html?interviewId= '+data.id + '">Details</a>'
-//
-//                    }
-//                }
-//            ]
-//    });
+//    <th>Id</th>
+//                    <th>Trainee</th>
+//                    <th>Scheduled Time</th>
+//                    <th>Trainer</th>
+//                    <th>Created Date</th>
+//                    <th>Office Name</th>
+//                    <th>Actions</th>
+    $('#trainer-table').DataTable({
+            ajax:{
+                "url": "/trainings",
+                "dataSrc": ""
+            },
+            columns :[
+                {"data": "id" },
+                {"data" : function(data,type,row,meta){
+                    let firstName = data.application.firstName;
+                    let lastName = data.application.lastName;
+                    let traineeName = lastName + " ," + firstName;
+                    return traineeName;
+                    },
+                    "defaultContent": ""},
+                {"data" : function(data,type,row,meta){
+                    if(data.scheduledTime == null){
+                        return " ";
+                    }
+                    return moment(data.scheduledTime).format('YYYY-MM-DD h:mm:ss a');
+                    },
+                    "defaultContent": ""},
+                {"data" : function(data,type,row,meta){
+                        if(data.trainer == null){
+                            return " ";
+                        }
+                        let firstName = data.trainer.firstName;
+                        let lastName = data.trainer.lastName;
+                        let trainerName = lastName + " ," + firstName;
+                        return trainerName;
+                    },
+                    "defaultContent":""},
+
+                {"data" : function(data,type,row,meta){
+                    if(data.application.office == null){
+                        return " ";
+                    }
+                    return data.application.office.name;
+                },
+                "defaultContent":""},
+                {"data" : function(data, type,row,meta){
+                    return moment(data.createdDate).format('YYYY-MM-DD h:mm:ss a');
+                    }
+                },
+                {   "targets": -1,
+                    "data": function(data, type,row,meta){
+                        return '<a class="btn btn-warning" href="/recruitment/training/training-details.html?trainingId= '+data.id + '">Details</a>'
+
+                    }
+                }
+            ]
+    });
 
 
 });
