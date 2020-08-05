7de270fe79fd654cafa05aa179a65b06a0538e18
Ken <saganawski@gmail.com>
2020-08-04 21:36:12 -0500

    remove staff diff direction

--- src/main/java/com/ks/management/staff/repository/JpaStaffRepo.java
+++
@@ -1,9 +0,0 @@
-package com.ks.management.staff.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ks.management.staff.Staff;
-
-public interface JpaStaffRepo extends JpaRepository<Staff, Integer> {
-
-}
