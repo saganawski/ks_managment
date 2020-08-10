ec7a566958d00803a0474012b00f664d8f9123eb
Ken <saganawski@gmail.com>
2020-08-10 17:59:44 -0500

    create common layout using css grid
    incorparte new lay out to application.html

--- src/main/resources/static/recruitment/application/js/application.js
+++ src/main/resources/static/recruitment/application/js/application.js
@@ -1,55 +1,66 @@
 $(document).ready(function(){
-    $('#application-table').DataTable({
-            "fnInitComplete": function (oSettings, json) {
-                $("div").removeClass("spinner-border");
-            },
-            ajax:{
-                "url": "/applications",
-                dataSrc: ''
-            },
-            columns :[
-                {"data" : "firstName"},
-                {"data" : "lastName"},
-                {"data" : "phoneNumber",
-                    "defaultContent":""},
-                {"data" : "email",
-                    "defaultContent":""},
-                {"data" : function(data, type,row,meta){
-                    if(data.dateReceived != null){
-                        return moment.utc(data.dateReceived).format('YYYY-MM-DD');
-                    }else{
-                        return " ";
-                    }
-                    },
-                    "defaultContent":""
-                },
-                {"data" : function(data, type,row,meta){
-                    if(data.callBackDate != null){
-                        return moment.utc(data.callBackDate).format('YYYY-MM-DD');
-                    }else{
-                        return " ";
-                    }
-                    },
-                    "defaultContent":""
-                },
-                {"data" : "applicationSource.source",
-                    "defaultContent":""},
-
-                {"data" : "applicationResult.result",
-                    "defaultContent":""},
-                {"data" : "office.name",
-                    "defaultContent":""},
-                {"data" : function(data, type,row,meta){
-                    return moment.utc(data.createdDate).format('YYYY-MM-DD h:mm:ss a');
-                    }
-                },
-                {   "targets": -1,
-                    "data": function(data, type,row,meta){
-                        return '<a class="btn btn-warning" href="/recruitment/application/application-edit.html?applicationId='+ data.id +'">Details</a>'
-                    }
-                }
-            ],
-            dom:"Bfrtip",
-            buttons: ['copy','csv','pdf']
+    const main = $('#load-layout').html();
+    $('#load-layout').load("/common/_layout.html", function(responseTxt, statusTxt, xhr){
+        if(statusTxt == "success"){
+            $('#load-layout').append(main);
+            setDatatable();
+        }
     });
+
+    function setDatatable(){
+        $('#application-table').DataTable({
+            responsive: true,
+                "fnInitComplete": function (oSettings, json) {
+                    $("div").removeClass("spinner-border");
+                },
+                ajax:{
+                    "url": "/applications",
+                    dataSrc: ''
+                },
+                columns :[
+                    {"data" : "firstName"},
+                    {"data" : "lastName"},
+                    {"data" : "phoneNumber",
+                        "defaultContent":""},
+                    {"data" : "email",
+                        "defaultContent":""},
+                    {"data" : function(data, type,row,meta){
+                        if(data.dateReceived != null){
+                            return moment.utc(data.dateReceived).format('YYYY-MM-DD');
+                        }else{
+                            return " ";
+                        }
+                        },
+                        "defaultContent":""
+                    },
+                    {"data" : function(data, type,row,meta){
+                        if(data.callBackDate != null){
+                            return moment.utc(data.callBackDate).format('YYYY-MM-DD');
+                        }else{
+                            return " ";
+                        }
+                        },
+                        "defaultContent":""
+                    },
+                    {"data" : "applicationSource.source",
+                        "defaultContent":""},
+
+                    {"data" : "applicationResult.result",
+                        "defaultContent":""},
+                    {"data" : "office.name",
+                        "defaultContent":""},
+                    {"data" : function(data, type,row,meta){
+                        return moment.utc(data.createdDate).format('YYYY-MM-DD h:mm:ss a');
+                        }
+                    },
+                    {   "targets": -1,
+                        "data": function(data, type,row,meta){
+                            return '<a class="btn btn-warning" href="/recruitment/application/application-edit.html?applicationId='+ data.id +'">Details</a>'
+                        }
+                    }
+                ],
+                dom:"Bfrtip",
+                buttons: ['copy','csv','pdf']
+        });
+    }
 });
