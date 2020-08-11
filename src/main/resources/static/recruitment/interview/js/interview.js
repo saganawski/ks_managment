5b1bfa8c8ad2939be1381fd7064977d3d500d4b3
Ken <saganawski@gmail.com>
2020-08-11 10:41:37 -0500

    add layout

--- src/main/resources/static/recruitment/interview/js/interview.js
+++ src/main/resources/static/recruitment/interview/js/interview.js
@@ -1,50 +1,60 @@
 $(document).ready(function(){
-    $('#interview-table').DataTable({
-        "initComplete": function(settings, json){
-            $("div").removeClass("spinner-border");
-        },
-            ajax:{
-                "url": "/interviews",
-                "dataSrc": ""
+    const main = $('#load-layout').html();
+    $('#load-layout').load("/common/_layout.html", function(responseTxt, statusTxt, xhr){
+        if(statusTxt == "success"){
+            $('#load-layout').append(main);
+            setDataTable();
+        }
+    });
+
+    function setDataTable(){
+        $('#interview-table').DataTable({
+            "initComplete": function(settings, json){
+                $("div").removeClass("spinner-border");
             },
-            columns :[
-                {"data": "id" },
-                {"data" : function(data,type,row,meta){
-                    let firstName = data.application.firstName;
-                    let lastName = data.application.lastName;
-                    let applicantName = lastName + " ," + firstName;
-                    return applicantName;
-                    },
-                    "defaultContent": ""},
-                {"data" : function(data,type,row,meta){
-                    if(data.scheduledTime == null){
-                        return " ";
-                    }
-                    return moment(data.scheduledTime).format('YYYY-MM-DD h:mm:ss a');
-                    },
-                    "defaultContent": ""},
-                {"data" : function(data,type,row,meta){
-                        if(data.interviewers == null){
+                ajax:{
+                    "url": "/interviews",
+                    "dataSrc": ""
+                },
+                columns :[
+                    {"data": "id" },
+                    {"data" : function(data,type,row,meta){
+                        let firstName = data.application.firstName;
+                        let lastName = data.application.lastName;
+                        let applicantName = lastName + " ," + firstName;
+                        return applicantName;
+                        },
+                        "defaultContent": ""},
+                    {"data" : function(data,type,row,meta){
+                        if(data.scheduledTime == null){
                             return " ";
                         }
-                        let interviewersLastNames = data.interviewers.map(interviewer => interviewer.lastName);
-                        return interviewersLastNames;
+                        return moment(data.scheduledTime).format('YYYY-MM-DD h:mm:ss a');
+                        },
+                        "defaultContent": ""},
+                    {"data" : function(data,type,row,meta){
+                            if(data.interviewers == null){
+                                return " ";
+                            }
+                            let interviewersLastNames = data.interviewers.map(interviewer => interviewer.lastName);
+                            return interviewersLastNames;
+                        },
+                        "defaultContent":""},
+                    {"data" : function(data, type,row,meta){
+                        return moment(data.createdDate).format('YYYY-MM-DD h:mm:ss a');
+                        }
                     },
-                    "defaultContent":""},
-                {"data" : function(data, type,row,meta){
-                    return moment(data.createdDate).format('YYYY-MM-DD h:mm:ss a');
-                    }
-                },
-                {   "targets": -1,
-                    "data": function(data, type,row,meta){
-                        return '<a class="btn btn-warning" href="/recruitment/interview/interview-details.html?interviewId= '+data.id + '">Details</a>'
+                    {   "targets": -1,
+                        "data": function(data, type,row,meta){
+                            return '<a class="btn btn-warning" href="/recruitment/interview/interview-details.html?interviewId= '+data.id + '">Details</a>'
 
+                        }
                     }
-                }
-            ],
-            dom:"Bfrtip",
-            buttons: ['copy','csv','pdf']
-    });
+                ],
+                dom:"Bfrtip",
+                buttons: ['copy','csv','pdf']
+        });
+    }
 
 
 });
