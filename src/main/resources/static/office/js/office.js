d1f69866d3283a453ce1fe6d22e1963be8b121e1
Ken <saganawski@gmail.com>
2020-08-11 18:53:43 -0500

    add layout

--- src/main/resources/static/office/js/office.js
+++ src/main/resources/static/office/js/office.js
@@ -1,22 +1,31 @@
 $(document).ready(function () {
-	// TODO: something to load side bar and nav
     var vm = this;
     vm.offices = [];
-    
-    $('#office-table').DataTable({
-        "initComplete": function(settings, json){
-            $("div").removeClass("spinner-border");
-        },
-    	ajax: {
-            url: "/offices",
-            dataSrc: ''
-        },
-        columns : [
-        	{"data": "name"},
-        	{"data": "location.address1",
-        	    "defaultContent":""
-        	}
-        ]
+
+    const main = $('#load-layout').html();
+    $('#load-layout').load("/common/_layout.html", function(responseTxt, statusTxt, xhr){
+        if(statusTxt == "success"){
+            $('#load-layout').append(main);
+            setDataTable();
+        }
     });
-    
+
+    function setDataTable(){
+        $('#office-table').DataTable({
+            "initComplete": function(settings, json){
+                $("div").removeClass("spinner-border");
+            },
+            ajax: {
+                url: "/offices",
+                dataSrc: ''
+            },
+            columns : [
+                {"data": "name"},
+                {"data": "location.address1",
+                    "defaultContent":""
+                }
+            ]
+        });
+    }
+
 });
