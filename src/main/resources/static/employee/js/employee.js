4658d45d528745fff47b3422781e9502f242f8bc
Ken <saganawski@gmail.com>
2020-08-11 19:00:38 -0500

    add layout

--- src/main/resources/static/employee/js/employee.js
+++ src/main/resources/static/employee/js/employee.js
@@ -1,42 +1,49 @@
 $(document).ready(function () {
-	// TODO: something to load side bar and nav
-
-    $('#employee-table').DataTable({
-        "initComplete": function(settings, json){
-            $("div").removeClass("spinner-border");
-        },
-        ajax:{
-            "url": "/employees",
-            "dataSrc": ""
-        },
-        columns :[
-            {"data" : "id"},
-            {"data" : "firstName"},
-            {"data" : "lastName"},
-            {"data" : "email"},
-            {"data" : "position.name",
-                "defaultContent":""},
-            {"data" : function(data,type,row,meta){
-                if(data.offices == undefined || data.offices.length === 0 ){
-                    return "";
-                }
-                let officeName = "";
-                for(office of data.offices){
-                    if(officeName.length ==0){
-                        officeName = office.name;
-                    }else{
-                        officeName = officeName + ", " + office.name
+    const main = $('#load-layout').html();
+    $('#load-layout').load("/common/_layout.html", function(responseTxt, statusTxt, xhr){
+        if(statusTxt == "success"){
+            $('#load-layout').append(main);
+            setDataTable();
+        }
+    });
+    function setDataTable(){
+        $('#employee-table').DataTable({
+            "initComplete": function(settings, json){
+                $("div").removeClass("spinner-border");
+            },
+            ajax:{
+                "url": "/employees",
+                "dataSrc": ""
+            },
+            columns :[
+                {"data" : "id"},
+                {"data" : "firstName"},
+                {"data" : "lastName"},
+                {"data" : "email"},
+                {"data" : "position.name",
+                    "defaultContent":""},
+                {"data" : function(data,type,row,meta){
+                    if(data.offices == undefined || data.offices.length === 0 ){
+                        return "";
+                    }
+                    let officeName = "";
+                    for(office of data.offices){
+                        if(officeName.length ==0){
+                            officeName = office.name;
+                        }else{
+                            officeName = officeName + ", " + office.name
+                        }
+                    }
+                    return officeName;
+                }, "defaultContent": ""},
+                {   "targets": -1,
+                    "data": function(data, type,row,meta){
+                        return '<a class="btn btn-warning" href="/employee/employee-details.html?employeeId='+ data.id +'">Details</a>'
                     }
                 }
-                return officeName;
-            }, "defaultContent": ""},
-            {   "targets": -1,
-                "data": function(data, type,row,meta){
-                    return '<a class="btn btn-warning" href="/employee/employee-details.html?employeeId='+ data.id +'">Details</a>'
-                }
-            }
-        ],
-        dom:"Bfrtip",
-        buttons: ['copy','csv','pdf']
-    });
+            ],
+            dom:"Bfrtip",
+            buttons: ['copy','csv','pdf']
+        });
+    }
 });
