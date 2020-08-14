ecc973255df9475cecf4f1d0a60341acedbb5cb3
Ken <saganawski@gmail.com>
2020-08-14 11:05:44 -0500

    create default users

---
+++ src/main/resources/db/migration/V14__default_users.sql
@@ -0,0 +1,6 @@
+
+
+insert into user (username,password,is_active,roles)
+    values
+        ('mike@kolehousestrategies.com','$2y$12$bwWcKse6jT25iH.3lHMIIuzGcGZU1zotgEXQepX5nzf2TLCZaVFiy', true, 'ADMIN'),
+        ('james@kolehousestrategies.com','$2y$12$bwWcKse6jT25iH.3lHMIIuzGcGZU1zotgEXQepX5nzf2TLCZaVFiy', true, 'ADMIN');
