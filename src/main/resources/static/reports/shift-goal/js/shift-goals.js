c8d013dbfa244c0555ed607049bc9872876e4ea2
Ken <saganawski@gmail.com>
2020-09-17 21:04:57 -0500

    stub out front end

---
+++ src/main/resources/static/reports/shift-goal/js/shift-goals.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    var vm = this;
+
+    const main = $('#load-layout').html();
+    $('#load-layout').load("/common/_layout.html", function(responseTxt, statusTxt, xhr){
+        if(statusTxt == "success"){
+            $('#load-layout').append(main);
+        }
+    });
+
+
+});
