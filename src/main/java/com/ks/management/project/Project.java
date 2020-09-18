67a08ad4e31b29f6776a369e9debd08f23da6496
Ken <saganawski@gmail.com>
2020-09-17 21:43:49 -0500

    add java classes

---
+++ src/main/java/com/ks/management/project/Project.java
@@ -0,0 +1,66 @@
+package com.ks.management.project;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.ks.management.office.Office;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name="project")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Project {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name="id")
+    private Integer id;
+
+    @Column(name="name")
+    private String name;
+
+    @Column(name = "shift_goal")
+    private Integer shiftGoal;
+
+    @Column(name = "shifts_scheduled")
+    private Integer shiftsScheduled;
+
+    @Column(name = "shifts_completed")
+    private Integer shiftCompleted;
+
+    @Column(name = "remaining_working_days")
+    private Integer remainingWorkingDays;
+
+    @Column(name = "shifts_needed")
+    private Integer shiftsNeeded;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.MERGE)
+    @JoinColumn(name = "office_id")
+    private Office office;
+
+    @Column(name = "updated_by")
+    private Integer updatedBy;
+
+    @Column(name = "updated_date", insertable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updatedDate;
+
+    @Column(name = "created_by")
+    private Integer createdBy;
+
+    @Column(name = "created_date", insertable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+    @OneToMany(mappedBy = "project_week",fetch = FetchType.LAZY,cascade = CascadeType.ALL)
+    private Set<ProjectWeek> projectWeeks = new HashSet<>();
+
+
+}
---
+++ src/main/java/com/ks/management/project/ProjectWeek.java
@@ -0,0 +1,60 @@
+package com.ks.management.project;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Date;
+
+@Entity
+@Table(name="project_week")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class ProjectWeek {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name="id")
+    private Integer id;
+
+    @Column(name = "original_shift_goal")
+    private Integer originalShiftGoal;
+
+    @Column(name = "current_shift_goal")
+    private Integer currentShiftGoal;
+
+    @Column(name="start_date")
+    private LocalDate startDate;
+
+    @Column(name="end_date")
+    private LocalDate endDate;
+
+    @Column(name = "shifts_scheduled")
+    private Integer shiftsScheduled;
+
+    @Column(name = "shifts_completed")
+    private Integer shiftCompleted;
+
+    @Column(name = "remaining_working_days")
+    private Integer remainingWorkingDays;
+
+    @Column(name = "shifts_needed")
+    private Integer shiftsNeeded;
+
+    @Column(name = "updated_by")
+    private Integer updatedBy;
+
+    @Column(name = "updated_date", insertable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updatedDate;
+
+    @Column(name = "created_by")
+    private Integer createdBy;
+
+    @Column(name = "created_date", insertable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+}
