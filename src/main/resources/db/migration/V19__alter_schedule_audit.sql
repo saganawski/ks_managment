6d1203e5de77a2a8c97fad4984ba77ad3604f4f3
Ken <saganawski@gmail.com>
2020-10-01 19:40:08 -0500

    add overtime_mintues

--- src/main/resources/db/migration/V19__alter_schedule_audit.sql
+++ src/main/resources/db/migration/V19__alter_schedule_audit.sql
@@ -1,3 +1,5 @@
 ALTER TABLE schedule_audit
 ADD COLUMN is_payroll tinyInt(1) DEFAULT FALSE AFTER office_id;
 
+ALTER TABLE employee_schedule_payroll
+ADD COLUMN overtime_minutes DECIMAL(6,2) DEFAULT NULL AFTER total_minutes;
