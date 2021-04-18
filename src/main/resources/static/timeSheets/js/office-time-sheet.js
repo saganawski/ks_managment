c933cab591bb94e74541461ec24b6aa7c6221f1c
Ken <saganawski@gmail.com>
2021-04-18 17:57:09 -0400

    add last name to title

--- src/main/resources/static/timeSheets/js/office-time-sheet.js
+++ src/main/resources/static/timeSheets/js/office-time-sheet.js
@@ -272,15 +272,17 @@ $(document).ready(function(){
         let events = [];
 
         for(schedule of employeeSchedules){
+            const fullName = schedule.employee.lastName + ", " + schedule.employee.firstName;
             event = {
                 id: schedule.id,
-                title: schedule.employee.lastName,
+                title: fullName,
                 start: schedule.scheduledTime,
                 allDay : true,
                 color : setColorByStatus(schedule.employeeScheduleStatus),
                 extendedProps: {
                     status: schedule.employeeScheduleStatus,
                     firstName: schedule.employee.firstName,
+                    lastName: schedule.employee.lastName,
                     employeeSchedule: schedule
                 }
             };
