b1195e9ffaf02dc003d8c77583a56a41d7764a5c
Ken <saganawski@gmail.com>
2024-02-05 22:20:55 -0500

    update dto to reflect new mileage pay rate

--- src/main/java/com/ks/management/report/ui/PayrollDto.java
+++ src/main/java/com/ks/management/report/ui/PayrollDto.java
@@ -24,7 +24,8 @@ public class PayrollDto {
     private BigDecimal miles;
     private BigDecimal mileagePay;
     private BigDecimal totalPay;
-
+    private final static double MILEAGE_PAY_RATE = 0.67;
+    private final static double OVERTIME_RATE = 1.5;
     public static BigDecimal calculateOvertimePay(BigDecimal overtimeRate, BigDecimal overtimeHours){
         if(!overtimeRate.equals(new BigDecimal(0.0)) && !overtimeHours.equals(new BigDecimal(0.0)) ){
             return  overtimeRate.multiply(overtimeHours).setScale(2,RoundingMode.CEILING);
@@ -46,7 +47,7 @@ public class PayrollDto {
 
     public static BigDecimal calculateOvertimeRate(BigDecimal totalOvertimeMinutes, BigDecimal payRate) {
         if(totalOvertimeMinutes != null && !totalOvertimeMinutes.equals(new BigDecimal(0.0))){
-            return payRate.multiply(new BigDecimal(1.5)).setScale(2,BigDecimal.ROUND_CEILING);
+            return payRate.multiply(new BigDecimal(OVERTIME_RATE)).setScale(2,BigDecimal.ROUND_CEILING);
         }else{
             return  new BigDecimal(0.0);
         }
@@ -71,7 +72,7 @@ public class PayrollDto {
 
     public static BigDecimal calculateMileagePay(BigDecimal totalMileage) {
         if(!totalMileage.equals(new BigDecimal(0.0))){
-            return totalMileage.multiply(new BigDecimal(0.56)).setScale(2,BigDecimal.ROUND_HALF_UP);
+            return totalMileage.multiply(new BigDecimal(MILEAGE_PAY_RATE)).setScale(2,BigDecimal.ROUND_HALF_UP);
         }
         return new BigDecimal(0.0);
     }
