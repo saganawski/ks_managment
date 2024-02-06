81cab225dc6b7e313d140e4e38a7c5dc2ce776c2
Ken <saganawski@gmail.com>
2024-02-06 11:21:02 -0500

    update bulk loader to look for office

--- src/main/resources/static/recruitment/application/js/bulk-application.js
+++ src/main/resources/static/recruitment/application/js/bulk-application.js
@@ -8,6 +8,21 @@ $(document).ready(function () {
             $('#load-layout').append(main);
             $("div").removeClass("spinner-border");
 
+            getOfficeOptions()
+                .then(function(data){
+                    console.log(data);
+                    setOfficeOptions(data);
+                    $("div").removeClass("spinner-border");
+                })
+                .fail(function(err){
+                    console.log(err);
+                    swal({
+                        title: "Error!",
+                        text: "Could not get Offices for drop down\n" + err.responseJSON.message,
+                        icon: "error"
+                    });
+                });
+
             const bulkForm = document.querySelector('#bulk-form');
             bulkForm.addEventListener('submit',function(event){
                 let validated = validationCheck(bulkForm);
@@ -60,6 +75,7 @@ $(document).ready(function () {
         if(!allowedExtensions.exec(filePath)){
             event.stopPropagation();
             form.classList.add('was-validated');
+            fileInput.classList.add('is-invalid');
              swal({
                 title: "Error!",
                 text: "Failure to upload file! \n" + "File must be type of .csv",
@@ -68,19 +84,25 @@ $(document).ready(function () {
             return false
         }
 
+        fileInput.classList.remove('is-invalid');
         form.classList.add('was-validated');
         return true;
 	}
 
-	function convertFormToJson(form){
-        let json = {};
-        for(let j of form){
-            json[j.name] = j.value.trim() || null;
+    function setOfficeOptions(offices){
+            for(office of offices){
+                $('#office').append("<option value='"+JSON.stringify(office)+"'>"+ office.name +"</option>")
+            }
         }
-        return json;
+
+    function getOfficeOptions(){
+        return $.ajax({
+            type:"GET",
+            url:"/offices"
+        });
     }
 
-    $('#load-layout').on("click", ".dropdown-item", function(event){
+/*    $('#load-layout').on("click", ".dropdown-item", function(event){
         event.preventDefault();
         const chosenType = $(this).data("value");
         vm.type = chosenType;
@@ -90,5 +112,5 @@ $(document).ready(function () {
         $(this).closest('.dropdown').hide();
         //show form
         $('#bulk-form').removeClass('hidden');
-    })
+    })*/
 });
