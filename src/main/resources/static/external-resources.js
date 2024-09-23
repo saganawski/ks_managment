71fc42b3c3689550d347d54a00963d2ad2c29510
Ken <saganawski@gmail.com>
2024-09-23 15:58:19 -0400

    begin consuladation of 3rd party libraries

---
+++ src/main/resources/static/external-resources.js
@@ -0,0 +1,21 @@
+// jQuery
+document.write('<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>');
+
+// Bootstrap
+document.write('<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">');
+document.write('<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"><\/script>');
+document.write('<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"><\/script>');
+
+// DataTables
+document.write('<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.css">');
+document.write('<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.js"></script>');
+
+// Moment plugin
+document.write('<script src="/pluggins/moment/moment.min.js"></script>');
+
+// Our Custom CSS
+document.write('<link rel="stylesheet" href="/common/style.css">');
+
+// Any new-user-specific resources would go here
+// For example:
+// document.write('<script src="/user/new-user-specific-library.js"><\/script>');
