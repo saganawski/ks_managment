bb5bf0ef0a7743ac0ffe26f6e0665d8bf47f8efd
Ken <saganawski@gmail.com>
2024-09-24 15:14:42 -0400

    consoldate 3rd party for user-details.html

--- src/main/resources/static/external-resources.js
+++ src/main/resources/static/external-resources.js
@@ -2,13 +2,14 @@
 document.write('<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>');
 // role-html and new-user.html used 3.5.1 jquery
 
-//	bootstrap-select
-document.write('<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.18/dist/css/bootstrap-select.min.css">');
-document.write('<script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.18/dist/js/bootstrap-select.min.js"></script>');
 // Bootstrap
 document.write('<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">');
 document.write('<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"><\/script>');
 document.write('<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"><\/script>');
+
+//	bootstrap-select
+document.write('<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.18/dist/css/bootstrap-select.min.css">');
+document.write('<script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.18/dist/js/bootstrap-select.min.js"></script>');
 
 // sweetAlert
 document.write('<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>')
@@ -25,5 +26,4 @@ document.write('<link rel="stylesheet" href="/common/style.css">');
 // Any new-user-specific resources would go here
 // For example:
 // document.write('<script src="/user/new-user-specific-library.js"><\/script>');
-
 
