633d82bc00d343770145242c4f8bc0dd2a8a4fbe
Ken Saganski <saganawski@gmail.com>
2025-06-07 13:16:12 -0400

    Consolidate third party scripts

--- src/main/resources/static/external-resources.js
+++ src/main/resources/static/external-resources.js
@@ -1,6 +1,7 @@
 // jQuery
-document.write('<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>');
-// role-html and new-user.html used 3.5.1 jquery
+// document.write('<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>');
+// Previous projects used version 3.4.1
+document.write('<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>');
 
 // Bootstrap
 document.write('<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">');
@@ -16,6 +17,17 @@ document.write('<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js
 // DataTables
 document.write('<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.css">');
 document.write('<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.js"></script>');
+// DataTables Buttons and export plugins
+document.write('<script src="https://cdn.datatables.net/buttons/1.6.2/js/dataTables.buttons.min.js"></script>');
+document.write('<script src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.flash.min.js"></script>');
+document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>');
+document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>');
+document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>');
+document.write('<script src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.html5.min.js"></script>');
+
+// FullCalendar
+document.write('<link href="https://cdn.jsdelivr.net/npm/fullcalendar@5.3.0/main.css" rel="stylesheet">');
+document.write('<script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.3.0/main.min.js"></script>');
 
 // Moment plugin
 document.write('<script src="/pluggins/moment/moment.min.js"></script>');
